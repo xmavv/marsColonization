de76b55e1d4e6adaaa207d68fe3a63365e716e26
Tesa44 <mateusz.tesarewicz123@gmail.com>
2024-12-29 14:20:57 +0100

    Added cors

--- BACKEND/src/app.js
+++ BACKEND/src/app.js
@@ -4,8 +4,11 @@ import { router as buildingRouter } from "./routes/buildingRoutes.js";
 import { router as resourceRouter } from "./routes/resourceRoutes.js";
 import { router as workerRouter } from "./routes/workerRoutes.js";
 import { router as taskRouter } from "./routes/taskRoutes.js";
+import cors from "cors";
+
 const app = express();
 
+// const cors = require('cors');
 app.use(json());
 app.use(
   urlencoded({
@@ -19,3 +22,19 @@ app.use("/api/v1/resources", resourceRouter);
 app.use("/api/v1/workers", workerRouter);
 app.use("/api/v1/tasks", taskRouter);
 export default app;
+
+// Configure CORS middleware
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PATCH','OPTIONS'], // Allowed methods
+  credentials: true,                  // Include cookies if needed
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+}));
+
+
