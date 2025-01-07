af67adcb2090695fdda25c4dc6c74fdbae858cf4
Tesa44 <mateusz.tesarewicz123@gmail.com>
2025-01-07 20:58:58 +0100

    Taking resources while increasing workers

--- BACKEND/src/controllers/buildingController.js
+++ BACKEND/src/controllers/buildingController.js
@@ -1,5 +1,14 @@
 import { pool } from "../db.js";
-import {MAX_BUILDING_LEVEL, RESOURCES,WORKERS, TYPES, JOBS, experience,production, updateCosts} from "../config.js";
+import {
+  MAX_BUILDING_LEVEL,
+  RESOURCES,
+  WORKERS,
+  TYPES,
+  JOBS,
+  experience,
+  production,
+  updateCosts,
+} from "../config.js";
 
 export const getBuildings = async function (req, res) {
   try {
@@ -24,132 +33,168 @@ export const getBuildings = async function (req, res) {
 
 export const getBuilding = async function (req, res) {
   try {
-    const userID = req.params.userid *1;
+    const userID = req.params.userid * 1;
     const type = req.params.type;
-    const [[data]] = await pool.query(`SELECT * FROM Buildings WHERE user_id = ? AND type = ?`,[userID,type]);
+    const [[data]] = await pool.query(
+      `SELECT * FROM Buildings WHERE user_id = ? AND type = ?`,
+      [userID, type]
+    );
     const level = data.level + 1;
-    const updateCost = getUpdateCost(type, level)
-    const product = getProduct(type, level)
- 
+    const updateCost = getUpdateCost(type, level);
+    const product = getProduct(type, level);
+
     res.status(200).json({
-      status: 'success',
+      status: "success",
       data: {
         data,
         updateCost,
         product,
-      }
-    })
-  }
-  catch (err) {
+      },
+    });
+  } catch (err) {
     res.status(404).json({
-      status: 'fail',
+      status: "fail",
       message: err,
-    })
+    });
   }
-}
+};
 
-const getUpdateCost = function(type, level) {
-  const cost = Object.entries(updateCosts.buildings[type])
-  const [,factor] = cost.pop()
-  const updateCost = {}
-  cost.forEach(([resource, value]) => updateCost[resource] = Math.ceil(factor**level*value));
-  return updateCost
-}
+const getUpdateCost = function (type, level) {
+  const cost = Object.entries(updateCosts.buildings[type]);
+  const [, factor] = cost.pop();
+  const updateCost = {};
+  cost.forEach(
+    ([resource, value]) =>
+      (updateCost[resource] = Math.ceil(factor ** level * value))
+  );
+  return updateCost;
+};
 
-const getProduct = function(type, level) {
-  const productEntries = Object.entries(production.buildings[type])
-  const [,productFactor] = productEntries.pop()
-  const [,time] = productEntries.pop()
-  const product = {} 
-  productEntries.forEach(([resource, value]) => product[resource] = Math.ceil(productFactor**level*value));
-  product["TIME"] = time
-  return product
-}
+const getProduct = function (type, level) {
+  const productEntries = Object.entries(production.buildings[type]);
+  const [, productFactor] = productEntries.pop();
+  const [, time] = productEntries.pop();
+  const product = {};
+  productEntries.forEach(
+    ([resource, value]) =>
+      (product[resource] = Math.ceil(productFactor ** level * value))
+  );
+  product["TIME"] = time;
+  return product;
+};
 
-export const checkType = function(req,res, next) {   
+export const checkType = function (req, res, next) {
   if (!TYPES.some((type) => type === req.params.type)) {
     return res.status(400).json({
-      status: 'fail',
-      message: 'Invalid Building Type',
-    })
+      status: "fail",
+      message: "Invalid Building Type",
+    });
   }
   next();
-}
+};
 
-export const updateBuildingLevel = async function(req, res) {
-    try {
-      await takeResources(req.params.userid, req.params.type, req.body.level)
-      const userLevel = await updateUserLevel(req.params.userid, {...req.params,...req.body})
-      const result = await pool.query('UPDATE Buildings SET level = ? WHERE user_id = ? AND type = ?',[req.body.level, req.params.userid,req.params.type])
-      res.status(200).json({
-        status: 'success',
-        message: 'Updated Building Level',
+export const updateBuildingLevel = async function (req, res) {
+  try {
+    const userID = req.params.userid * 1;
+    const type = req.params.type;
+    const level = req.body.level; //Building level
+    await takeResources(userID, type, level);
+    const userLevel = await updateUserLevel(userID, type, level);
+    const result = await pool.query(
+      "UPDATE Buildings SET level = ? WHERE user_id = ? AND type = ?",
+      [level, userID, type]
+    );
+    const [[{ id }]] = await pool.query(
+      "SELECT id FROM Buildings WHERE user_id = ? AND type = ?",
+      [userID, type]
+    );
+    const updateCost = getUpdateCost(req.params.type, req.body.level);
+    const product = getProduct(req.params.type, req.body.level);
+    res.status(200).json({
+      status: "success",
+      message: "Updated Building Level",
+      data: {
         data: {
-            type: req.params.type,
-            level: req.body.level,
-            userlevel: userLevel,
+          id,
+          user_id: userID,
+          type: type,
+          level: level,
+          user_level: userLevel,
         },
-      })
-    }
-    catch (err) {
-      res.status(400).json({
-        status: 'fail',
-        message: err,
-      })
-    }
-}
+        updateCost,
+        product,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
 
-export const checkBody = function(req,res,next) {
+export const checkBody = function (req, res, next) {
   const level = req.body.level * 1;
   if (!(level >= 0 && level <= MAX_BUILDING_LEVEL)) {
     return res.status(400).json({
-      status: 'fail',
-      message: 'Invalid Level Value'
-    })
+      status: "fail",
+      message: "Invalid Level Value",
+    });
   }
-  next()
-}
+  next();
+};
 
-const updateUserLevel = async function(userID, body) {
-  try{
-    const buildingLevel = body.level
-    const type = body.type
-    let [[{level}]] = await pool.query("SELECT level FROM Users WHERE id = ?",[userID])
-    level += experience.buildings[type].INCREASE_FACTOR**buildingLevel*experience.buildings[type].EXP
-    level = Math.round(level / 100) * 100 //Round to hundreds
-    await pool.query("UPDATE Users SET level = ? WHERE id = ?",[level,userID])
-    return level 
+const updateUserLevel = async function (userID, type, buildingLevel) {
+  try {
+    let [[{ level }]] = await pool.query(
+      "SELECT level FROM Users WHERE id = ?",
+      [userID]
+    );
+    level +=
+      experience.buildings[type].INCREASE_FACTOR ** buildingLevel *
+      experience.buildings[type].EXP;
+    level = Math.round(level / 100) * 100; //Round to hundreds
+    await pool.query("UPDATE Users SET level = ? WHERE id = ?", [
+      level,
+      userID,
+    ]);
+    return level;
+  } catch (err) {
+    throw "Failed updating user level";
   }
-  catch (err) {
-      throw "Failed updating user level"
-  }
-}
+};
 
-const takeResources = async function(userID,type, level) {
-  try{
-    const updateCost = getUpdateCost(type, level)
-    const [[curValues]] = await pool.query("SELECT * FROM Resources r INNER JOIN Workers w ON r.user_id = w.user_id WHERE w.user_id = ?",[userID])
-    const newValues = {}
-    // Object.entries(updateCost).forEach(([resource, value]) => newResources[resource.toLowerCase()] = curResources[resource.toLowerCase()] - value)
+const takeResources = async function (userID, type, level) {
+  try {
+    const updateCost = getUpdateCost(type, level);
+    const [[curValues]] = await pool.query(
+      "SELECT * FROM Resources r INNER JOIN Workers w ON r.user_id = w.user_id WHERE w.user_id = ?",
+      [userID]
+    );
+    const newValues = {};
     for (let [key, value] of Object.entries(updateCost)) {
-        key = key.toLowerCase()
-        if (RESOURCES.includes(key)) {
-            const newValue = curValues[key] - value;
-            newValues[key] = newValue
-            await pool.query(`UPDATE Resources SET ?? = ? WHERE user_id = ?`,[key,newValue,userID])
-        }
-        else if (key === 'workers') {
-          key = JOBS[type]
-          const newValue = curValues[key] - value;
-          newValues[key] = newValue
-          await pool.query(`UPDATE Workers SET ?? = ? WHERE user_id = ?`,[key,value,userID]);
-        }
+      key = key.toLowerCase();
+      if (RESOURCES.includes(key)) {
+        const newValue = curValues[key] - value;
+        newValues[key] = newValue;
+        await pool.query(`UPDATE Resources SET ?? = ? WHERE user_id = ?`, [
+          key,
+          newValue,
+          userID,
+        ]);
+      } else if (key === "workers") {
+        key = JOBS[type];
+        const newValue = curValues[key] - value;
+        newValues[key] = newValue;
+        await pool.query(`UPDATE Workers SET ?? = ? WHERE user_id = ?`, [
+          key,
+          value,
+          userID,
+        ]);
+      }
     }
-    console.log(newValues)
+    console.log(newValues);
+  } catch (err) {
+    throw "Failed updating resources";
   }
-  catch (err) {
-    throw "Failed updating resources"
-  }
-}
-
-
+};
--- BACKEND/src/controllers/workerController.js
+++ BACKEND/src/controllers/workerController.js
@@ -1,86 +1,128 @@
-import { buyCosts, experience, WORKERS } from '../config.js';
-import {pool} from '../db.js';
+import { buyCosts, experience, WORKERS } from "../config.js";
+import { pool } from "../db.js";
 
 export const getWorkers = async function (req, res) {
-    try {
-        const userID = req.params.userid *1;
-        const [[data]] = await pool.query(`SELECT * FROM Workers WHERE user_id = ?`,[userID])
-        const buyCosts = getBuyCosts()
-        res.status(200).json({
-            status: 'success',
-            data: {
-                data,
-                buyCosts,
-            }
-        })
-    }
-    catch (err) {
-        res.status(404).json({
-            status: 'fail',
-            message: err,
-        })
-    }
+  try {
+    const userID = req.params.userid * 1;
+    const [[data]] = await pool.query(
+      `SELECT * FROM Workers WHERE user_id = ?`,
+      [userID]
+    );
+    const buyCosts = getBuyCosts();
+    res.status(200).json({
+      status: "success",
+      data: {
+        data,
+        buyCosts,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: err,
+    });
+  }
 };
 
-const getBuyCosts = function() {
-    return buyCosts.workers
-}
-
-export const updateWorkers = async function(req, res) {
-    try {
-        const userID = req.params.userid * 1;
-        const userLevel = await updateUserLevel(userID,req.body);
-        const entries = Object.entries(req.body)
-        entries.forEach(async ([worker, value]) => await pool.query(`UPDATE Workers SET ?? = ? WHERE user_id = ?`,[worker,value,userID]));
-        let [[data]] = await pool.query("SELECT * FROM Workers WHERE user_id = ?",[userID])
-        data = {...data,...req.body}
-        data["userlevel"] = userLevel
-        res.status(200).json({
-            status: 'success',
-            message: 'Updated Workers',
-            data: {
-                data,
-            }
-        })
+export const checkBody = function (req, res, next) {
+  try {
+    const entries = Object.entries(req.body);
+    if (
+      !entries.every(
+        ([worker, value]) =>
+          WORKERS.includes(worker) &&
+          Number.isInteger(value * 1) &&
+          value * 1 >= 0
+      )
+    ) {
+      return res.status(400).json({
+        status: "fail",
+        message: "Invalid Worker or Value",
+      });
     }
-    catch (err) {
-        res.status(400).json({
-            status: 'fail',
-            message: err,
-        })
-    }
+  } catch (err) {
+    return res.status(400).json({
+      status: "fail",
+      message: err,
+    });
+  } finally {
+    next();
+  }
 };
 
-export const checkBody = function(req, res, next) {
-    try {
-        const entries = Object.entries(req.body);
-        if (!(entries.every(([worker,value]) => WORKERS.includes(worker) && Number.isInteger(value*1) && value*1 >= 0))) {
-            return res.status(400).json({
-                status: 'fail',
-                message: 'Invalid Worker or Value'
-            })
-        }
-    }
-    catch (err) {
-        return res.status(400).json({
-            status: 'fail',
-            message: err
-        })
-    }
-    finally {
-        next();
-    }
-}
+const getBuyCosts = function () {
+  return buyCosts.workers;
+};
 
-const updateUserLevel = async function(userID, body) {
-    const [[curWorkers]] = await pool.query("SELECT * FROM Workers WHERE user_id = ?",[userID])
-    let [[{level}]] = await pool.query("SELECT level FROM Users WHERE id = ?",[userID])
-    for (const [worker, value] of Object.entries(body)) {
-        const diff = value - curWorkers[worker];
-        if (diff > 0) {
-            level += diff*experience.workers[worker].EXP;
-        }
+export const updateWorkers = async function (req, res) {
+  try {
+    const userID = req.params.userid * 1;
+    const [[curWorkers]] = await pool.query(
+      "SELECT * FROM Workers WHERE user_id = ?",
+      [userID]
+    );
+    const entries = Object.entries(req.body);
+    const userLevel = await updateUserLevel(userID, entries, curWorkers);
+    const newCoins = await takeResources(userID, entries, curWorkers);
+    entries.forEach(
+      async ([worker, value]) =>
+        await pool.query(`UPDATE Workers SET ?? = ? WHERE user_id = ?`, [
+          worker,
+          value,
+          userID,
+        ])
+    );
+    let [[data]] = await pool.query("SELECT * FROM Workers WHERE user_id = ?", [
+      userID,
+    ]);
+    data = { ...data, ...req.body };
+    data["userlevel"] = userLevel;
+    data["coins"] = newCoins;
+    res.status(200).json({
+      status: "success",
+      message: "Updated Workers",
+      data: {
+        data,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
+
+const updateUserLevel = async function (userID, entries, curWorkers) {
+  let [[{ level }]] = await pool.query("SELECT level FROM Users WHERE id = ?", [
+    userID,
+  ]);
+  for (const [worker, value] of entries) {
+    const diff = value - curWorkers[worker];
+    if (diff > 0) {
+      level += diff * experience.workers[worker].EXP;
     }
-    await pool.query("UPDATE Users SET level = ? WHERE id = ?",[level,userID])
-    return level
-}
+  }
+  await pool.query("UPDATE Users SET level = ? WHERE id = ?", [level, userID]);
+  return level;
+};
+
+const takeResources = async function (userID, entries, curWorkers) {
+  let totalCost = 0;
+  for (const [worker, value] of entries) {
+    const diff = value - curWorkers[worker];
+    if (diff > 0) {
+      totalCost += diff * buyCosts.workers[worker].COINS;
+    }
+  }
+  const [[{ coins }]] = await pool.query(
+    "SELECT coins FROM Resources WHERE user_id = ?",
+    [userID]
+  );
+  const newCoins = coins - totalCost;
+  await pool.query("UPDATE Resources SET coins = ? WHERE user_id = ?", [
+    newCoins,
+    userID,
+  ]);
+  return newCoins;
+};
